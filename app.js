84ec60a4701395c3e7b0387c4afe43c430464ed8
suchanoot <far_suchanoot@hotmail.com>
2016-08-20 22:03:23 +0700

    hello

---
+++ app.js
@@ -0,0 +1,26 @@
+/*global Vue*/
+var app = new Vue({
+  el: '#app',
+  data: {
+  msg: 'Pokemon Ranger',
+  list : [],
+  },
+  methods: {
+    getApi: function () {
+      var url = 'http://www.pokesnipers.com/api/v1/pokemon.json'
+      var that = this
+        this.$http.get(url).then(function (res) {
+        this.list = res.data.results
+        console.log(that.list)
+      })
+    }
+  },
+  ready: function (){
+    this.getApi()
+    var that = this
+    setInterval(function(){
+    that.getApi()
+    },5000) // calback
+  }
+})
+console.log(app)
